617bb7f46744e1256d4b45dde25df5a027c4e221
RodrigoS147 <rodrisantos147@gmail.com>
2023-05-09 14:10:09 -0300

    Agrego funcion Edit en EspecialidadController.cs

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -21,5 +21,23 @@ namespace Turnos.Controllers
             return View(_context.Especialidad.ToList());
         }
 
+        public IActionResult Edit (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var especialidad = _context.Especialidad.Find(id);
+
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(especialidad);
+        }
+
+        
     }
 }
