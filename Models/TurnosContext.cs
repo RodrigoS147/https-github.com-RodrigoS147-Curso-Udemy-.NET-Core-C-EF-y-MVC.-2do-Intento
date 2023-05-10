c0e19c027cb5d9b8da8afbd3eca60dcafff4acf9
RodrigoS147 <rodrisantos147@gmail.com>
2023-05-10 00:38:14 -0300

    Actualizacion

--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -14,5 +14,25 @@ namespace Turnos.Models
 
         public DbSet<Especialidad> Especialidad { get; set; }
 
+        public DbSet<Paciente> Paciente { get; set; }
+
+          protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Especialidad>(entidad => 
+            {
+                entidad.ToTable("Especialidad");
+
+                entidad.HasKey(e => e.IdEspecialidad);
+
+                entidad.Property(e => e.Descripcion)
+                .IsRequired()
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            }
+            );
+        }
+
     }
+
+      
 }
