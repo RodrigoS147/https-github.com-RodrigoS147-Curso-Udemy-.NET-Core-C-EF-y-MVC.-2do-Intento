8355e0fe8d21d42a1a2ea456d6cd3b60a2ac4457
RodrigoS147 <rodrisantos147@gmail.com>
2023-05-12 15:22:27 -0300

    Correccion del error con la creacion de Paciente

--- Models/TurnosContext.cs
+++ Models/TurnosContext.cs
@@ -13,7 +13,7 @@ namespace Turnos.Models
         }
 
         public DbSet<Especialidad> Especialidad { get; set; }
-        
+
         public DbSet<Paciente> Paciente { get; set; }
 
           protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -30,6 +30,40 @@ namespace Turnos.Models
                 .IsUnicode(false);
             }
             );
+
+              modelBuilder.Entity<Paciente>(entidad =>
+            {
+                entidad.ToTable("Paciente");
+
+                entidad.HasKey(p => p.IdPaciente);
+
+                entidad.Property(p => p.Nombre)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entidad.Property(p => p.Apellido)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+                entidad.Property(p => p.Direccion)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+                entidad.Property(p => p.Telefono)
+                .IsRequired()
+                .HasMaxLength(20)
+                .IsUnicode(false);
+
+                entidad.Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            }
+        );
+
         }
 
     }
